454a9e4354ca1f34e18a1d5da7d5d3d30c33b067
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-06-02 18:16:55 +0200

    reprise du site en angular

--- profile/src/app/app.module.ts
+++ profile/src/app/app.module.ts
@@ -7,20 +7,32 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu'; 
 import {MatIconModule} from '@angular/material/icon';
-import { WhoamiComponent } from './whoami/whoami.component'; 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { WhoamiComponent } from './whoami/whoami.component';
+import { FormationComponent } from './formation/formation.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { FooterComponent } from './footer/footer.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WhoamiComponent
+    WhoamiComponent,
+    FormationComponent,
+    ExperienceComponent,
+    ProjetsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
