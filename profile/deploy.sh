d2e38b1f8f74cc2c54beb452f8ea0460af3d3e8e
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2020-06-02 18:25:44 +0200

    deployment

---
+++ profile/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm run build 
+
+cp -r dist/profile/. ../deployprofile/anagonousourou.github.io
+
+cd ../deployprofile/anagonousourou.github.io
+git add -A
+git commit -m "deployment"
+git push
