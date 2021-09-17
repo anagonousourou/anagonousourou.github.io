0913d99d0e45fabbb1deae7ebd02630d264e696e
ANAGONOU Sourou Patrick <anagonousourou@gmail.com>
2021-09-17 13:07:15 +0200

    deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ npm run build
 
 cp -r dist/profile/. ../deployprofile/anagonousourou.github.io
 
-cd ../deployprofile/anagonousourou.github.io
+git checkout master
+cp -r ../deployprofile/anagonousourou.github.io/. .
 git add -A
 git commit -m "deployment"
 git push
